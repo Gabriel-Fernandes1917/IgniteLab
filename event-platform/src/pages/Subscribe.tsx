d444ca6d33d18a15c0e64b57f540fa7f4ae32287
Gabriel Fernandes <gabgui2001@gmail.com>
2022-06-26 00:30:06 -0300

    class 4

--- event-platform/src/pages/Subscribe.tsx
+++ event-platform/src/pages/Subscribe.tsx
@@ -1,11 +1,45 @@
+import { useState, FormEvent } from "react";
 import { Logo } from "../componentes/Logo";
+import {gql, useMutation} from "@apollo/client"
+import { useNavigate } from "react-router-dom";
+
+const CREATE_SUBSCRIBER_MUTATION = gql`
+    mutation CreateSubscriber($name: String!, $email: String!) {
+        createSubscriber(data: {name: $name, email: $email}) {
+        id
+  }
+}
+
+`
+
 
 export function Subscribe() {
+
+    const navigate = useNavigate()
     
+    const [name, setName]= useState('');
+    const [email, setEmail]= useState('');
+
+    const [createSubscriber, {loading}] = useMutation(CREATE_SUBSCRIBER_MUTATION)
+
+
+   async function handleSubscribe(event: FormEvent) {
+        event.preventDefault();
+
+        await createSubscriber({
+            variables:{
+                name,
+                email
+            }
+        })
+
+        navigate('/evento')
+    }
+
     return(
         <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
-            <div className="max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
-                <div className="max-w-[64px]">
+            <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
+                <div className="max-w-[640px]">
                     <Logo />
 
                     <h1 className="mt-8 text-[2.5rem] leading-tight">
@@ -14,6 +48,37 @@ export function Subscribe() {
                     <p className="mt-4 text-gray-200 leading-relaxed">
                         Em apenas uma semana você vai dominar na prática uma das tecnologias mais utilizadas e com alta demanda para acessar as melhores oportunidades do mercado.
                     </p>
+                </div>
+
+                <div className="p-8 bg-gray-700 border border-gray-500 rounded">
+                    <strong className="text-2xl mb-7 block">Inscreva-se no evento</strong>
+
+                    <form onSubmit={handleSubscribe} className="flex flex-col gap-2 w-full">
+                        <input
+                        className="bg-gray-900 rounded px-5 h-14"
+                         type="text"
+                         placeholder="Seu nome e sobrenome"
+                         onChange={event => setName(event.target.value)}
+                           />
+                        
+                        <input
+                        className="bg-gray-900 rounded px-5 h-14"
+                         type="text"
+                         placeholder="Seu E-mail"
+                         onChange={event => setEmail(event.target.value)}
+                           />
+
+                        <button
+                         type="submit"
+                         disabled={loading} // disable with loading true
+                         className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors disabled:opacity-50"
+                         
+                         >
+                            Garantir a chave do sucesso
+                        </button>
+
+                    </form>
+
                 </div>
             </div>
 
